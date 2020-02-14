686fac6ab5b34cf2755471c98c9bafbb85e67954
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-02-14 16:32:50 +0100

    fix: Expose filter and take.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const collect = require("./collect/collect");
 const collectInArray = require("./collect/collectInArray");
 const collectInSet = require("./collect/collectInSet");
+const filter = require("./transform/filter");
 const range = require("./create/range");
+const take = require("./cut/take");
 
 module.exports = {
   collect,
   collectInArray,
   collectInSet,
-  range
+  filter,
+  range,
+  take
 };

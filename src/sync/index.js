01ad4402dc953adc31456065c30955f2f0db0763
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-02-16 15:30:06 +0100

    chore: Order exports in sync index by submodule.

--- src/sync/index.js
+++ src/sync/index.js
@@ -4,56 +4,73 @@
  * @module g:sync
  */
 
-const acknowledgable = require("./control/acknowledgable");
-const chain = require("./transform/chain");
-const concat = require("./create/concat");
+// Consume
 const collect = require("./consume/collect");
 const collectInArray = require("./consume/collectInArray");
 const collectInSet = require("./consume/collectInSet");
+const forEach = require("./consume/forEach");
+const reduce = require("./consume/reduce");
+
+// Control
+const acknowledgable = require("./control/acknowledgable");
+const retryable = require("./control/retryable");
+
+// Create
+const concat = require("./create/concat");
 const cycle = require("./create/cycle");
+const from = require("./create/from");
+const iterate = require("./create/iterate");
+const range = require("./create/range");
+const repeat = require("./create/repeat");
+
+// Remove
 const drop = require("./remove/drop");
 const dropRepeats = require("./remove/dropRepeats");
 const dropWhile = require("./remove/dropWhile");
 const filter = require("./remove/filter");
-const find = require("./search/find");
-const from = require("./create/from");
 const head = require("./remove/head");
-const iterate = require("./create/iterate");
-const map = require("./transform/map");
-const range = require("./create/range");
-const reduce = require("./consume/reduce");
-const repeat = require("./create/repeat");
-const retryable = require("./control/retryable");
-const some = require("./search/some");
 const tail = require("./remove/tail");
 const take = require("./remove/take");
 const takeWhile = require("./remove/takeWhile");
-const zip = require("./transform/zip");
 
+// Search
+const find = require("./search/find");
+const some = require("./search/some");
+
+// Transform
+const chain = require("./transform/chain");
+const map = require("./transform/map");
 module.exports = {
-  acknowledgable,
-  chain,
-  concat,
   collect,
   collectInArray,
   collectInSet,
+  forEach,
+  reduce,
+
+  acknowledgable,
+  retryable,
+
+  concat,
   cycle,
+  from,
+  iterate,
+  range,
+  repeat,
+
   drop,
   dropRepeats,
   dropWhile,
   filter,
-  find,
-  from,
   head,
-  iterate,
-  map,
-  range,
-  reduce,
-  repeat,
-  retryable,
-  some,
   tail,
   take,
   takeWhile,
+
+  find,
+  some,
+  chain,
+  map,
   zip
 };
+
+const zip = require("./transform/zip");
